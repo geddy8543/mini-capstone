e2c81d61f4171a01a59a3c1d7ab8358f795aecff
Gail Eddy <gaileddy68@gmail.com>
2021-10-13 11:55:05 -0500

    added products to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+product1 = Product.create(name: "t-shirts plain", price: "10", image_url: "https://images.pexels.com/photos/5274988/pexels-photo-5274988.jpeg?auto=compress&cs=tinysrgb&h=650&w=940", description: "t_shirts with no design")
+product2 = Product.create(name: "dress shirts", price: "30", image_url: "https://images.pexels.com/photos/2780196/pexels-photo-2780196.jpeg?auto=compress&cs=tinysrgb&h=650&w=940", description: "button-down shirts you can wear to work")
+product3 = Product.create(name: "shoes", price: "50", image_url: "https://images.pexels.com/photos/4098358/pexels-photo-4098358.jpeg?auto=compress&cs=tinysrgb&h=650&w=940", description: "a variety of shoes for all your needs")
